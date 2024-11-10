07d7e2e58c3ba0a58446ddfc1d95d193bd3edaea
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-11-10 18:14:06 +0100

    removeAllContacts added

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,15 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+    try {
+        const db = [];  
+        writeContacts(db);
+        console.log("All contacts removed.");
+      } catch (error) {
+        console.error('Error removing contact: ', error);
+      }
+};
 
 removeAllContacts();
+
+
