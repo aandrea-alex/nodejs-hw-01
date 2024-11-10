f0fa006fce66a624ce7414211e4bafa4d1836fba
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-11-10 17:48:27 +0100

    addOneContact added

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,4 +1,18 @@
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+    try {
+        const db = await readContacts();  
+        const newContact = createFakeContact();  
+         
+        db.push(newContact);
+        writeContacts(db);
+        console.log("New contact added.");
+      } catch (error) {
+        console.error('Error adding contact: ', error);
+      }
+};
 
 addOneContact();
