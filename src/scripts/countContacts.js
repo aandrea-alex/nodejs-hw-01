3553bcb6f8648de70f857ca8026ad42070c6b78f
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-11-10 17:56:47 +0100

    added function countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+   const db = await readContacts();
+   return db.length;
+};
 
 console.log(await countContacts());
